4d8f69eec5ccb8e68aafc70a54d0cdf6df8e8e9f
Yury Yurochko <yurochkoyura@gmail.com>
2021-07-29 09:13:28 +0400

    feat(step3): sending data via udp

--- src/fiber.lua
+++ src/fiber.lua
@@ -1,14 +1,18 @@
 local fiber = require('fiber')
 local log = require('log')
+local socket = require('socket')
+local msgpack = require('msgpack')
 
 messages = fiber.channel(32)
 
 fiber.create(function()
-	local count = 0
+	local udp = socket('AF_INET', 'SOCK_DGRAM', 'udp')
+
 	while true do
 		log.info("sender is waiting")
-		m = messages:get()
-		count = count + 1
-		log.info("sender got %d messages", count)
+		message = messages:get()
+		packet = msgpack.encode(message)
+		udp:sendto('192.168.1.77', 30000, packet)
+		log.info("sender got and sent message")
 	end
 end)
